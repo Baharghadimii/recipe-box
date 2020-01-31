cfc49af98e59d9490a0df16e7fbb160517c7c3b8
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-31 13:38:46 -0800

    modify button style

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,8 +6,7 @@
 
       <RecipeList />
 
-      <b-button variant="primary" href="#">Do Something</b-button>
-      <b-button variant="success" href="#">Do Something Else</b-button>
+      <b-button class="btn-dash" href="#">Add New Recipe</b-button>
     </b-jumbotron>
   </div>
 </template>
