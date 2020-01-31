5b08f860ee8091af4d0c700a0c1d71ed06196454
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-31 15:27:21 -0800

    add style to recipe

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Recipe</h1>
+  <div class="mt-4">
+    <h4>Recipe Name</h4>
+    <b-card
+      img-src="https://placekitten.com/300/300"
+      img-alt="Card image"
+      img-left
+      class="mb-3"
+    >
+      <b-card-text>
+        Recipe Ingredients
+      </b-card-text>
+      <b-card-text>
+        Recipe Instruction
+      </b-card-text>
+    </b-card>
+  </div>
 </template>
