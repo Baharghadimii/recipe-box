a68ee9e096622e867a84f2fecb27bc179a9eaa6f
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-31 23:56:33 -0800

    display number of recipes in profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <b-jumbotron header="Bahareh Ghadimi" lead="Number of Recipes: ">
+    <b-jumbotron header="Bahareh Ghadimi">
+      <p>Number of recipes: {{ numRecipe }}</p>
       <b-button variant="primary" href="/">back</b-button>
     </b-jumbotron>
   </div>
 </template>
 <script>
-export default {};
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {
+      numRecipe: 0
+    };
+  },
+  computed: {
+    ...mapState(["recipes"])
+  },
+  mounted() {
+    const numberOfRecipes = this.recipes.length;
+    this.numRecipe = numberOfRecipes;
+  }
+};
 </script>
